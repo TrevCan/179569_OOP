ce047f4e5de39f254ba93fa52f4df4f90dd4c9bb
a <a@example.org>
2023-08-07 10:27:20 -0600

    add hello world program

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+//Hello World program
+//a program that prints
+//Hello, world!
+//
+//by Hector Canizales
+
+public class HelloWorld {
+
+	public static void main(String[] args){
+
+		System.out.println("Hello, world!");
+
+	}
+
+}
